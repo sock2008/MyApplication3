06aca91591b42c3e2ac5181688db8469b39ad4ce
Yuri Lapin <sock.ops@gmail.com>
2018-08-04 00:52:37 +0400

    first fix (test)

--- app/src/main/java/com/example/yura/myapplication/MainActivity.java
+++ app/src/main/java/com/example/yura/myapplication/MainActivity.java
@@ -4,18 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+//    String login = "admin.foto";
+//    String pass = "magdj530";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void loginButton(View view){
 
-        String login = String  getText(R.id.login);
+    public void loginButton(View view) {
+        EditText loginEt = findViewById(R.id.login);
+        String login = loginEt.getText().toString();
+        EditText passEt = findViewById(R.id.pass);
+        String pass = passEt.getText().toString();
+        if((login.equals("admin.foto")) && (pass.equals("magdj530"))){
+            logMess(login + pass);}
+            else {
+            logMess("Отказано в доступе");
+        }
 
+    }
+
+    public void logMess(String message) {
+        TextView logResult = findViewById(R.id.login_result);
+        logResult.setText(message);
     }
 
 

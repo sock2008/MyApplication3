af08a47816488a8c1039715c0069f2e120669dda
Yuri Lapin <sock.ops@gmail.com>
2018-08-03 23:46:29 +0400

    Initial commit

---
+++ app/src/main/java/com/example/yura/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.yura.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void loginButton(View view){
+
+        String login = String  getText(R.id.login);
+
+    }
+    public  void s
+
+
+}

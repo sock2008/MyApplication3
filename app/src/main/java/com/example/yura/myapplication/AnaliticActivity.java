7799c8b027d0f3b82fba1ae03da2b40b9ae4e2cb
Yuri Lapin <sock.ops@gmail.com>
2018-08-04 14:34:37 +0400

    commit after unknou git huy znaet and add AnalAct

--- app/src/main/java/com/example/yura/myapplication/AnaliticActivity.java
+++ app/src/main/java/com/example/yura/myapplication/AnaliticActivity.java
@@ -1,15 +1,13 @@
 package com.example.yura.myapplication;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-
-public class AppActivity extends Activity {
+public class AnaliticActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_app);
+        setContentView(R.layout.activity_analitic);
     }
 }
